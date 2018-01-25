30d6c9e765ef8d3e5949d9bcf02e4cf1f3079311
kmansel <kmansel@spu.edu>
2018-01-25 15:30:30 -0800

    update server code

--- server/main.go
+++ server/main.go
@@ -1,64 +1,46 @@
+/*
+ *
+ * Copyright 2018 Samsung SDS Cloud Native Computing Team authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
 import (
 	"flag"
 	"log"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	 pb "../api"
-	"google.golang.org/grpc/reflection"
-	"strings"
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"crypto/tls"
-	"crypto/x509"
-	"../certs"
-	"google.golang.org/grpc/credentials"
 	"net/http"
-	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"net"
+	"google.golang.org/grpc/reflection"
 )
 
 var (
-  	serverAddr = flag.String("server_addr", "127.0.0.1:8155", "The server address in the format of host:port")
-	demoKeyPair  *tls.Certificate
-	demoCertPool *x509.CertPool
+  	serverAddrGRPC = flag.String("server_addr_grpc", "127.0.0.1:5300", "The server address in the format of host:port")
+	serverAddrREST = flag.String("server_addr_rest", "127.0.0.1:6300", "The REST endpoint address, in the format of host:port")
 )
 
 // server is used to implement helloworld.GreeterServer.
 type server struct{}
-
-// grpcHandlerFunc returns an http.Handler that delegates to grpcServer on incoming gRPC
-// connections or otherHandler otherwise. Copied from cockroachdb.
-func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// This is a partial recreation of gRPC's internal checks https://github.com/grpc/grpc-go/pull/514/files#diff-95e9a25b738459a2d3030e1e6fa2a718R61
-		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
-			grpcServer.ServeHTTP(w, r)
-		} else {
-			fmt.Printf("\n\ntrying to serve http 1.1 request \nreponse writer: %+v \nrequest: %+v", w, r)
-			otherHandler.ServeHTTP(w, r)
-		}
-	})
-}
-
-func setupCerts() {
-	pair, err := tls.X509KeyPair([]byte(certs.Cert), []byte(certs.Key))
-	if err != nil {
-		panic(err)
-	}
-	demoKeyPair = &pair
-	demoCertPool = x509.NewCertPool()
-	ok := demoCertPool.AppendCertsFromPEM([]byte(certs.Cert))
-	if !ok {
-		panic("bad certs")
-	}
-}
-
 
 // GetPoseidon implements helloworld.GreeterServer
 func (s *server) GetPoseidon(ctx context.Context, in *pb.HelloPoseidon) (*pb.PoseidonReply, error) {
@@ -71,32 +53,10 @@ func (s *server) GetPoseidonAgain(ctx context.Context, in *pb.HelloPoseidon) (*p
 
 func main() {
 
-	setupCerts()
-
 	log.Println("Server starting")
 
-	opts := []grpc.ServerOption{grpc.Creds(credentials.NewClientTLSFromCert(demoCertPool, *serverAddr))}
-
-	grpcServer := grpc.NewServer(opts...)
+	grpcServer := grpc.NewServer()
 	pb.RegisterPingPoseidonServer(grpcServer, &server{})
-	ctx := context.Background()
-
-	dcreds := credentials.NewTLS(&tls.Config{
-		ServerName: *serverAddr,
-		RootCAs:    demoCertPool,
-	})
-
-	dopts := []grpc.DialOption{grpc.WithTransportCredentials(dcreds)}
-
-	mux := http.NewServeMux()
-	gwmux := runtime.NewServeMux()
-	err := pb.RegisterPingPoseidonHandlerFromEndpoint(ctx, gwmux, *serverAddr, dopts)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	mux.Handle("/", gwmux)
-
 
 	//  Get notified that server is being asked to stop
 	// Handle SIGINT and SIGTERM.
@@ -114,23 +74,29 @@ func main() {
 	    os.Exit(0)
 	}()
 
-	conn, err := net.Listen("tcp", *serverAddr)
+	log.Printf("Setting up GPRC to listen on : %v", *serverAddrGRPC)
+
+	conn, err := net.Listen("tcp", *serverAddrGRPC)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	srv := &http.Server{
-		Addr: *serverAddr,
-		Handler: grpcHandlerFunc(grpcServer, mux),
-		TLSConfig: &tls.Config{
-			Certificates: []tls.Certificate{*demoKeyPair},
-			NextProtos:   []string{"h2"},
-		},
-	}
-
 	// Register reflection service on gRPC server.
 	reflection.Register(grpcServer)
-	if err := srv.Serve(tls.NewListener(conn, srv.TLSConfig)); err != nil {
+	go grpcServer.Serve(conn)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	dialOpts := []grpc.DialOption{grpc.WithInsecure()}
+	err = pb.RegisterPingPoseidonHandlerFromEndpoint(ctx, mux, *serverAddrGRPC, dialOpts)
+	if err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
+	log.Printf("Setting up and Serving REST on : %v", *serverAddrREST)
+
+	http.ListenAndServe(*serverAddrREST, mux)
 }
